a093b190b6e9d5ddb6b6dc12a1c140418d62608f
m4yoNICE <cenizaroswellrey@gmail.com>
2026-04-19 21:05:34 +0800

    changed link

--- src/App.jsx
+++ src/App.jsx
@@ -265,7 +265,7 @@ export default function App() {
             </p>
             <div className="download-btns">
               <a
-                href="https://expo.dev/artifacts/eas/oDvuBxtBqSoem2meRUZYwM.apk"
+                href="https://expo.dev/artifacts/eas/bQm3WRy5sfyTxdJKKzn26z.apk"
                 download
                 className="dl-btn dl-btn--apk"
               >
